f69ce78890de6acc58c94ab6091711b9dfad37cf
bbartschi14 <bbartschi14@gmail.com>
2023-01-09 16:13:50 -0500

    Updated server to bundle.js path

--- server/src/server.ts
+++ server/src/server.ts
@@ -75,11 +75,12 @@ app.use("/api", api);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 // load the compiled react files, which will serve /index.html and /bundle.js
-const reactPath = path.resolve(__dirname, "..", "client", "dist");
+const reactPath = path.resolve(__dirname, "../..", "client", "dist");
 app.use(express.static(reactPath));
 
 // for all other routes, render index.html and let react router handle it
 app.get("*", (req, res) => {
+  console.log(reactPath);
   res.sendFile(path.join(reactPath, "index.html"));
 });
 
